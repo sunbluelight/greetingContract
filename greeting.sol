7ec37d108a1257dbd7ff27375317097714f5bf90
sunbluelight <sunbluelight@proton.me>
2025-08-21 10:57:33 +0100

    Create greeting.sol
    My first smart contract.

---
+++ greeting.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract helloWorld{
+
+    function greeting() pure public returns(string memory){
+        return("Hello world!");
+    }
+}
